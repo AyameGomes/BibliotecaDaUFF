0088f831f71489f7a4e45e8dd0a759808cfbeed7
AyameGomes <ayamegomes@gmail.com>
2022-11-05 19:43:09 -0300

    Commit inicial

---
+++ src/jframe/FecharPrograma.java
@@ -0,0 +1,16 @@
+package jframe;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ayame
+ */
+public class FecharPrograma {
+    public static void fechar() {
+        System.exit(0);
+    }
+}
